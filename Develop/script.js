295474237e0bd294cb9aa5bdd105ed65dbd65850
Patricia <124640646+P-Trish@users.noreply.github.com>
2023-03-10 20:35:02 -0800

    added pw length code

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,33 @@
 // Assignment Code
+var length;
 var generateBtn = document.querySelector("#generate");
+
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var specialCharacters = "!@#$%^&*()_+=?";
+var numbers = "1234567890";
+var userChoice = "";
+var password = "";
+
 
 // Write password to the #password input
 function generatePassword () {
-  var length = prompt ("Please choose a length for password.");
+  length = prompt ("Please choose a length for password.");
   if (length < 8 || length > 128 || isNaN(length)  ) {
   alert ("Please chooose a number between 8 and 128 characters.");    
   generatePassword(); 
   }
 
-}
+  userPreference()
+  }
+
+  function userPreference () {
+    upperCase = confirm ("Do you want Uppercase letters?")
+    if (upperCase === "yes") {
+
+    }
+  }
+
 
 // CONFIRMS for upper/lower case and/or special characters 
 
@@ -24,3 +42,12 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+
+// PSEUDO-CODE:
+// when asked for character types to include in the password, I confirm whether or not to include lowercase, uppercase, numbers, and/or special characters 
+
+// confirm lowercase
+// confirm uppercase
+// confirm numbers
+// confirm special characters 
